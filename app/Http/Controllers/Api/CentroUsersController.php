c0dbb8cb6c08721ada8a62bf121684a3f72b2ca5
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-13 17:42:35 +0000

    Add: disableauthorization para centrousers

--- app/Http/Controllers/Api/CentroUsersController.php
+++ app/Http/Controllers/Api/CentroUsersController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Centro;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Http\Request;
+use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\Controller as ControllersController;
 use Orion\Http\Controllers\RelationController;
 
 class CentroUsersController extends RelationController
 {
+    use DisableAuthorization;
     // use DisableAuthorization;
     protected $model = Centro::class;
     protected $relation = 'users';
