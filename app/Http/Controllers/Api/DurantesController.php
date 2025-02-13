edf8f2f9480d8ce94508a815b8d96e5012742382
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-13 10:11:10 +0000

    Fix: uth guard [api] is not defined

--- app/Http/Controllers/Api/DurantesController.php
+++ app/Http/Controllers/Api/DurantesController.php
@@ -9,9 +9,6 @@ use Orion\Http\Controllers\Controller;
 class DurantesController extends Controller
 {
     use DisableAuthorization;
-
-
-    use DisableAuthorization;
     protected $model = Durante::class; // or "App\Models\Post"
 
 }
