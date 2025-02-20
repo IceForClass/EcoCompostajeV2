16cfe73b1c9f5514cdf6d4c1399042d3862130be
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-20 20:02:38 +0000

    Add: Todo lo necesario para el login
    Por ahora solo está para traer desde localhost

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -11,33 +11,17 @@ use Illuminate\Http\Request;
 
 class AuthenticatedSessionController extends Controller
 {
-    public function store(LoginRequest $request): JsonResponse
+    public function store(LoginRequest $request)
     {
-        // 1) Autenticar
         $request->authenticate();
 
-        // 2) (Opcional) Regenerar sesión (más usado si usarás cookies/sesión).
-        //    Puedes quitarlo si vas a usar SOLAMENTE tokens y no cookies.
         $request->session()->regenerate();
 
-        // 3) Obtener el usuario autenticado
-        // ó $user = $request->user();
-
-        // 4) Generar el token con Sanctum
-        $tokenResult = $request->user()->createToken('myTokenName');
-        $token = $tokenResult->plainTextToken;
-
-        // 5) Devolver respuesta con el token
-        return response()->json([
-            'message' => 'Login exitoso',
-            'token'   => $token,
-            'user'    => Auth::user(),
-        ]);
+        return response()->noContent();
     }
 
     public function destroy(Request $request): Response
     {
-        // Aquí puedes también revocar el token o simplemente cerrar sesión
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
