218deab6ab1d87ef63673d87a52c229b3cf7e429
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-21 16:35:24 +0000

    Add: Intentos para el login

--- config/sanctum.php
+++ config/sanctum.php
@@ -18,7 +18,7 @@ return [
 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', implode(',', [
     'localhost', 'localhost:5173', 'localhost:5174',
     '127.0.0.1', '127.0.0.1:3000', '127.0.0.1:8000', '::1',
-    'navet21.github.io'
+    'https://navet21.github.io'
 ]))),
 
 
--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,8 @@ use Orion\Facades\Orion;
 // Ruta protegida con Sanctum para obtener el usuario autenticado
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
 
 Route::get('sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
 
