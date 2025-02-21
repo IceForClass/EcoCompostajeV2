8413ec44aa58fab69ea3dcce5a9751b6ad8f76eb
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-21 11:25:07 +0000

    Revert "Fix: Intento de login"
    This reverts commit 88d64dcb5e10f38eb3b2cb67c022ae772e1c4a0b.

--- config/cors.php
+++ config/cors.php
@@ -18,14 +18,10 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['GET', "POST", "PATCH", "PUT", "DELETE"],
-    
-    // 'allowed_origins' => ['*'],
-    
-    // 'allowed_origins_patterns' => [],
 
-    'allowed_origins' => ['https://navet21.github.io'],
-
-    'allowed_origins_patterns' => ['/^https:\/\/navet21\.github\.io\/frontcompostaje/'],
+    'allowed_origins' => ['*'],
+    
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
