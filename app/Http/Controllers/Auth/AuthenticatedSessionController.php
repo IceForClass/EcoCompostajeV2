6fc09cf001969f10d162a9dff96f27b5ece12726
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-24 16:43:41 +0000

    no se que pasa
    Dani si esto sale mal es tu culpa
    
    Te vas a ir a mexico
    
    Maldito Danny!
    
    Te odio danny
    
    Co-Authored-By: Danny belloli <151564103+danny79k@users.noreply.github.com>

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,41 +4,33 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
-    public function store(LoginRequest $request): JsonResponse
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): Response
     {
-        // 1) Autenticar
         $request->authenticate();
 
-        // 2) (Opcional) Regenerar sesión (más usado si usarás cookies/sesión).
-        //    Puedes quitarlo si vas a usar SOLAMENTE tokens y no cookies.
         $request->session()->regenerate();
 
-        // 3) Obtener el usuario autenticado
-        // ó $user = $request->user();
-
-        // 4) Generar el token con Sanctum
-        $tokenResult = $request->user()->createToken('myTokenName');
-        $token = $tokenResult->plainTextToken;
-
-        // 5) Devolver respuesta con el token
-        return response()->json([
-            'message' => 'Login exitoso',
-            'token'   => $token,
-            'user'    => Auth::user(),
-        ]);
+        return response()->noContent();
     }
 
+    /**
+     * Destroy an authenticated session.
+     */
     public function destroy(Request $request): Response
     {
         Auth::guard('web')->logout();
+
         $request->session()->invalidate();
+
         $request->session()->regenerateToken();
 
         return response()->noContent();
