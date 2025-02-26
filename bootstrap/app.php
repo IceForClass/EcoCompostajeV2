64fef03f78c668cfdfe104dc5d23a57f8090c96a
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-26 19:51:53 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,13 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             StartSession::class,
         ]);
-
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
