0ff27b9b4e847ba16097c3199c8523ad26e8b390
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-26 19:49:09 +0000

    Revert "Update app.php"
    This reverts commit 497be6f5524d86df666b7d0a660c9ad59f43a974.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,10 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
             StartSession::class,
         ]);
 
-        // Habilitar CORS y CSRF
-        $middleware->validateCsrfTokens(except: [
-            'https://navet21.github.io/frontcompostaje', // Cambia por tu dominio de GitHub Pages
-        ]);
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
 
         $middleware->alias([
