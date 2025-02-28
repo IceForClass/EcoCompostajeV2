34acc8dd51282c1426f39966c28a189fc29db699
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-28 19:28:09 +0000

    Bye Bye factories

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,28 +10,6 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Crear usuario admin
-        $admin = User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            "password" => bcrypt('123456789'),
-            "admin" => true,
-        ]);
-        $Federico = User::factory()->create([
-            'name' => 'Federico',
-            'email' => 'federico@gmail.com',
-            "password" => bcrypt('Federico1234'),
-            "admin" => true,
-        ]);
-        $Lidia = User::factory()->create([
-            'name' => 'Lidia',
-            'email' => 'lidia@gmail.com',
-            "password" => bcrypt('Lidia1234'),
-            "admin" => true,
-        ]);
-        $admin->centros()->attach([5 => ['admin' => true]]);
-        $Federico->centros()->attach([1 => ['admin' => true]]);
-        $Lidia->centros()->attach([2 => ['admin' => true], 3 => ['admin' => true], 4 => ['admin' => true]]);
 
         // Crear 9 usuarios adicionales y asignarles centros aleatorios
         // User::factory(9)->create()->each(function ($user) {
