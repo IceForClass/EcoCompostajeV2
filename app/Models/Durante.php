c7fc99c361e87efa7cd38a4fab5ddd00be8132b9
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-03-04 19:26:19 +0000

    Add: Olor en el fillable

--- app/Models/Durante.php
+++ app/Models/Durante.php
@@ -10,7 +10,7 @@ class Durante extends Model
     /** @use HasFactory<\Database\Factories\DuranteFactory> */
     use HasFactory;
 
-    protected $fillable = ['registro_id', 'riego', 'remover', 'aporte_verde','aporte_seco', 'cantidad_aporteVLitros', 'cantidad_aporteVKilos','tipo_aporteV','cantidad_aporteSLitros', 'cantidad_aporteSKilos','tipo_aporteS', 'foto', 'observaciones'];
+    protected $fillable = ['registro_id', 'riego', 'remover', 'aporte_verde','aporte_seco', 'cantidad_aporteVLitros', 'cantidad_aporteVKilos','tipo_aporteV','cantidad_aporteSLitros', 'cantidad_aporteSKilos','tipo_aporteS', 'olor', 'foto', 'observaciones'];
 
     public function registro(){
         return $this->belongsTo(Registro::class,'registro_id');
